052a8c472063af9196e519056f9be97ed238a6c5
Gustavo Cardoso <gugamin@bol.com.br>
2021-02-28 19:37:36 -0300

    end

---
+++ README.md
@@ -0,0 +1,38 @@
+## yarn init -y
+## yarn add express
+## yarn add @types/express -D
+## yarn tsc --init
+## yarn add ts-node-dev -D
+
+<!-- https://typeorm.io/#/ -->
+## yarn add typeorm reflect-metadata
+## yarn add sqlite3
+
+
+## yarn typeorm migration:create -n CreateUsers
+
+## yarn typeorm migration:run
+## yarn typeorm migration:show
+## yarn typeorm migration:revert
+
+ <!-- Realizar testes -->
+## yarn add jest @types/jest -D
+## yarn add ts-jest -D
+## yarn add supertest @types/supertest -D
+
+<!-- enviar email -->
+## yarn add nodemailer
+## yarn add @types/nodemailer -D
+
+<!-- template -->
+## yarn add handlebars
+
+<!-- variaveis de ambiente -->
+## yarn add dotenv
+
+<!-- validações -->
+## yarn add yap
+
+<!-- Tratar erros -->
+## yarn add express-async-errors
+## import "express-async-errors"
---
+++ src/services/SendMailService.ts
@@ -0,0 +1,52 @@
+import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
+
+class SendMailService {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then((account) => {
+      // Create a SMTP transporter object
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
+  public async execute(
+    to: string,
+    subject: string,
+    variables: object,
+    path: string
+  ) {
+    // const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+    const templateFileContent = fs.readFileSync(path).toString("utf8");
+
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      text: "Hello to myself!",
+      html,
+      from: "NPS <noreplay@nps.com.br>",
+    });
+
+    console.log("Message sent: %s", message.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export default new SendMailService();
